611ef230977e0a264ed2ba24c68a34f3b2eafa22
Christian Daconta <cdaconta@gmail.com>
2020-12-17 08:26:44 -0500

    Added token variable to functions of auth0

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -134,8 +134,8 @@ def create_app(test_config=None):
 
 
   @app.route('/index')
-  @requires_auth
-  def get_all_fighters():
+  @requires_auth()
+  def get_all_fighters(token):
       #Here I get all the fighters by division
       div_1 = Fighter.query.filter(Fighter.division == 1).order_by(Fighter.rank).all()
       div_1_data = [item.format() for item in div_1]
@@ -190,13 +190,13 @@ def create_app(test_config=None):
                                 userinfo_pretty=json.dumps(session[constants.JWT_PAYLOAD], indent=4), events = event_info, div_1 = div_1_data, div_2 = div_2_data, div_3 = div_3_data, div_4 = div_4_data, div_5 = div_5_data, div_6 = div_6_data, div_7 = div_7_data, div_8 = div_8_data, div_9 = div_9_data, div_10 = div_10_data, div_11 = div_11_data, div_12 = div_12_data)
 
   @app.route('/knockouts')
-  @requires_auth
-  def get_knockout_page():
+  @requires_auth()
+  def get_knockout_page(token):
     return render_template('knockouts.html',  userinfo=session[constants.PROFILE_KEY])
 
   @app.route('/division_fighters/<int:division_id>')
-  @requires_auth
-  def get_division_fighters(division_id):
+  @requires_auth()
+  def get_division_fighters(token, division_id):
     #Here I get all the fighters - questions = Question.query.filter(Question.category==category_id).all()
     division_fighters = Fighter.query.filter(Fighter.division == division_id).order_by(Fighter.rank).all()
 
@@ -219,8 +219,8 @@ def create_app(test_config=None):
                               userinfo_pretty=json.dumps(session[constants.JWT_PAYLOAD], indent=4), fighters = data, names=names) #events = event_info)
   
   @app.route('/event/<date>')
-  @requires_auth
-  def get_event(date):
+  @requires_auth()
+  def get_event(token, date):
     clean_date = html.unescape(date)
     event_info = []
     #event_data = Event.query.order_by(Event.event_date.desc()).limit(6)
@@ -254,14 +254,14 @@ def create_app(test_config=None):
                                 userinfo_pretty=json.dumps(session[constants.JWT_PAYLOAD], indent=4), events = event_info, divisions = division_info)
  
   @app.route('/event/create', methods=['GET']) 
-  @requires_auth  
-  def create_event_form():
+  @requires_auth()  
+  def create_event_form(token):
     form = EventForm()
     return render_template('forms/new_event.html', form=form, userinfo=session[constants.PROFILE_KEY])
   
   @app.route('/event/create', methods=['POST'])
-  @requires_auth
-  def create_event():
+  @requires_auth()
+  def create_event(token):
     try:
       # get form data and create 
       form = EventForm()
@@ -339,8 +339,8 @@ def create_app(test_config=None):
                 }), 200
 
   @app.route('/event/<date>', methods=['DELETE'])
-  @requires_auth
-  def delete_event(date):
+  @requires_auth()
+  def delete_event(token, date):
     events = Event.query.filter(Event.event_date == date).all()
 
     events.delete()
@@ -351,8 +351,8 @@ def create_app(test_config=None):
 
 
   @app.route('/fighter/edit/<int:fighter_id>', methods=['GET']) 
-  @requires_auth  
-  def fighter_edit_form(fighter_id):
+  @requires_auth()  
+  def fighter_edit_form(token, fighter_id):
     
     fighter = Fighter.query.get(fighter_id)
     #fighter_details = Fighter.format(fighter)
@@ -364,8 +364,8 @@ def create_app(test_config=None):
     return render_template('forms/edit_fighter.html', form=form, fighters = fighter_details, userinfo=session[constants.PROFILE_KEY])
   
   @app.route('/fighter/edit/<int:fighter_id>', methods=['POST'])
-  @requires_auth
-  def edit_fighters(fighter_id):
+  @requires_auth()
+  def edit_fighters(token, fighter_id):
       fighter_division = 0
       try:
         fighter = Fighter.query.filter(Fighter.id == fighter_id).one_or_none()
