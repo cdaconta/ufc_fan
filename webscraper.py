e3527fdc51e4f9e065c5b1be7c885347505d0b7b
cdaconta <cdaconta@gmail.com>
2021-02-16 14:42:25 -0500

    Moved location of implicit wait

--- webscraper.py
+++ webscraper.py
@@ -8,8 +8,9 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-driver.get("https://www.ufc.com/trending/all")
 driver.implicitly_wait(10) # seconds
+driver.get("https://www.ufc.com/trending/all")
+
 try:
     heading3 = driver.find_elements_by_tag_name("h3")
     for item in heading3:   
