ed7a87f9ecf4bef75d741739780e1593450e0706
cdaconta <cdaconta@gmail.com>
2021-02-16 10:08:46 -0500

    Added double quotes to selector in find element

--- webscraper.py
+++ webscraper.py
@@ -9,7 +9,7 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 driver.get("https://www.ufc.com/trending/all")
-heading3 = driver.find_element_by_tag_name('h3')
+heading3 = driver.find_element_by_tag_name("h3")
 for item in heading3:   
     print(heading3[item])
 print("Finished!")
