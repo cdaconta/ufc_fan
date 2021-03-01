5542ca4afc9a2a0d49e288c8a464fb1671ea156e
Christian Daconta <cdaconta@gmail.com>
2021-03-01 14:10:27 -0500

    Changeds to get_elements function

--- webscraper.py
+++ webscraper.py
@@ -13,10 +13,10 @@ driver.implicitly_wait(10) # seconds
 driver.get("https://www.ufc.com/trending/all")
 
 def get_elements():
-    heading3 = driver.find_elements_by_tag_name("h3").get_attribute("textContent")
+    heading3 = driver.find_elements_by_tag_name("h3")
 
     for item in heading3:
-        print(item.text)
+        print(item.get_attribute("textContent"))
 def test():
     print("testing")
 
